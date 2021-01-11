d7c920085b50fad3e17d4c2610045b70d0d41831
atharva12 <atharvaanantkulkarni.com>
2021-01-11 23:10:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+//DEtecting button press
+var n = document.querySelectorAll(".drum").length;
+for(var i = 0;i<n;i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click",  function (){
+
+    var buttonName = this.innerHTML;
+    play(buttonName);
+    buttonAnimation(buttonName);
+  });
+}
+
+//we used a class .drum here becoz in our html files there can also be other buttons
+// var audio = new Audio('sounds/tom-1.mp3'); audio.play();
+
+
+//DEtecting button press
+document.addEventListener("keypress",function (event){
+  var buttonName = event.key;
+  play(buttonName);
+  buttonAnimation(buttonName);
+});
+
+
+
+//function of what happenns when key or button is pressed/clicked
+
+function play(buttonName){
+  switch (buttonName) {
+    case "w":
+       var audio = new Audio('sounds/tom-1.mp3'); audio.play();
+      break;
+    case "a":
+      var audio = new Audio('sounds/tom-2.mp3'); audio.play();
+      break;
+    case "s":
+      var audio = new Audio('sounds/tom-3.mp3'); audio.play();
+      break;
+    case "d":
+      var audio = new Audio('sounds/tom-4.mp3'); audio.play();
+      break;
+    case "j":
+      var audio = new Audio('sounds/snare.mp3'); audio.play();
+      break;
+    case "k":
+      var audio = new Audio('sounds/crash.mp3'); audio.play();
+      break;
+    case "l":
+      var audio = new Audio('sounds/kick-bass.mp3'); audio.play();
+      break;
+    default:
+
+  }
+}
+
+
+
+function buttonAnimation(buttonName){
+  document.querySelector("."+buttonName).classList.add("pressed");  //we added pressed class to buttons after they are being clicked to create a shadow
+  setTimeout(function(){
+    document.querySelector("."+buttonName).classList.remove("pressed");
+  },100);
+}
